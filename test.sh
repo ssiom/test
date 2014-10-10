cdf022eb2111e0355f1f14cb51b328ff10fe61a3
Fabien Carrion <fabien.carrion@gmail.com>
2014-10-09 20:34:14 -0500

    my second try

--- test.sh
+++ test.sh
@@ -1,3 +1,10 @@
 #! /bin/sh
 
-
+for host in pi@192.168.4.5 fabs@192.168.4.4
+  do echo $host 
+  for a in $(ssh $host df -h | grep -v Filesystem | awk '{print $1 }'| sort | uniq)
+    do b=$(ssh $host df -h | grep $a | awk '{ print $2}')
+    c=$(ssh $host mount | grep $a | awk '{ print $5 }')
+    echo $a $b $c
+  done 
+done
